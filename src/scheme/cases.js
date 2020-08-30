20c5bd509b70dc67c9241fef3e4decdae20f516d
Kochkin Danila <>
2020-08-30 22:03:16 +0300

    feat(): Cases

---
+++ src/scheme/cases.js
@@ -0,0 +1,24 @@
+const cases = [
+  {
+    img: 'Background.jpg',
+    title: 'Case 1',
+    author: 'Nadya'
+  },
+  {
+    img: 'Title.jpg',
+    title: 'Case 2',
+    author: 'Nadya'
+  },
+  {
+    img: 'Main.jpg',
+    title: 'Case 3',
+    author: 'Nadya'
+  },
+  {
+    img: 'Description.jpg',
+    title: 'Case 4',
+    author: 'Nadya'
+  }
+]
+
+export default cases
--- src/sections/Cases/Cases.jsx
+++ src/sections/Cases/Cases.jsx
@@ -1,9 +1,34 @@
-import { Layout } from '../../components'
+import { Box, GridList, GridListTile, GridListTileBar } from '@material-ui/core'
 
-const Cases = () => (
-  <Layout>
-    <h1>Cases</h1>
-  </Layout>
-)
+import { Layout } from '../../components'
+import { cases } from '../../scheme'
+
+const Cases = () => {
+  return (
+    <Layout>
+      <Box p={2}>
+        <GridList
+          cols={2}
+        >
+          {cases.map((caseItem) => (
+            <GridListTile
+              key={caseItem.img}
+              rows={2}
+            >
+              <img
+                src={require(`../../../public/images/${caseItem.img}`)}
+                alt={caseItem.title}
+              />
+              <GridListTileBar
+                title={caseItem.title}
+                subtitle={<span>by: {caseItem.author}</span>}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </Box>
+    </Layout>
+  )
+}
 
 export default Cases
